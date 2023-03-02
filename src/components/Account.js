7192494446126f57e890ab1da91f01087df05f2e
nirgluzman <nir.gluzman@gmail.com>
2023-03-02 23:44:29 +0100

    add onAuthStateChanged - Authentication state observer

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,12 @@
+import { UserAuth } from "../context/AuthContext";
+
 const Account = () => {
+  const { user, logout } = UserAuth();
+
   return (
     <div className="max-w-[600px] mx-auto my-16 p-4">
       <h1 className="text-2xl font-bold py-4">Account</h1>
-      <p>User Email:</p>
+      <p>User Email: {user && user.email}</p>
       <button className="border px-6 py-2 my-4">Logout</button>
     </div>
   );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import {
   createUserWithEmailAndPassword,
@@ -12,16 +12,34 @@ import { auth } from "../firebase";
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  // Sign up new users
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  // Sign in existing users
   const loginUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  // Sign out users
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  // Authentication state observer
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("currentUser=", currentUser);
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <UserContext.Provider value={{ createUser, loginUser }}>
+    <UserContext.Provider value={{ user, createUser, loginUser, logout }}>
       {children}
     </UserContext.Provider>
   );
