40194594ec65fdd78edbdb1044ea65858c2e61b9
nirgluzman <nir.gluzman@gmail.com>
2023-03-02 23:51:11 +0100

    add logout

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { UserAuth } from "../context/AuthContext";
 
 const Account = () => {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   const { user, logout } = UserAuth();
+
+  const handleLogout = async () => {
+    setError("");
+    try {
+      await logout();
+      navigate("/");
+      console.log("You are logged out");
+    } catch (err) {
+      setError(err.message);
+      console.log(err.message);
+    }
+  };
 
   return (
     <div className="max-w-[600px] mx-auto my-16 p-4">
       <h1 className="text-2xl font-bold py-4">Account</h1>
       <p>User Email: {user && user.email}</p>
-      <button className="border px-6 py-2 my-4">Logout</button>
+      <button onClick={handleLogout} className="border px-6 py-2 my-4">
+        Logout
+      </button>
     </div>
   );
 };
