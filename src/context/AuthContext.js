c4be6962d237c71e90e8e0d8b38294107a75e7d6
nirgluzman <nir.gluzman@gmail.com>
2023-03-02 23:04:54 +0100

    typo fix in AuthContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,9 +3,9 @@ import { createContext, useContext } from "react";
 const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  return <UserContext.Provider value={}></UserContext.Provider>;
+  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
 };
 
 export const UserAuth = () => {
-    return useContext(UserContext);
-  };
+  return useContext(UserContext);
+};
